e2b2b28c27febf334e56d3c843130067b8d3b054
gabby <gabriellakachura@gmail.com>
2025-10-19 23:16:18 +0300

    all primitive types

---
+++ DataTypes/src/AllPrimitiveTypes.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// Task 19
+//Въпроси: как е нов ред?
+//как да принтирам всичките в едно sout
+public class AllPrimitiveTypes {
+    public static void main(String[] args) {
+     byte a = 1;
+     short b = 2;
+     int c = 3;
+     long d = 4;
+     float e = 5.21F;
+     double f = 6.346554;
+     char g = 'G';
+     boolean bool = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(bool);
+
+    }
+}
